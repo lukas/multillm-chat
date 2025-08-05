653b93ae2bae828467847cfe07a7b9eba705ee5f
lbiewald <lbiewald@catnip.run>
2025-08-05 16:09:25 +0000

    Localhost Configuration checkpoint: 1

--- mock-web-server.js
+++ mock-web-server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 const PORT = 3007;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
--- web-server.js
+++ web-server.js
@@ -15,7 +15,7 @@ const io = socketIo(server, {
 });
 
 const PORT = 3007;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
